3c2a1720fbf15528991f30b044e81c09adc377f6
yun02 <yun02190219@gmail.com>
2025-01-14 15:49:54 +0900

    🎨 #7 로그인 페이지 스타일 수정 - 요소 가운데 정렬

--- frontend/src/Pages/Login.js
+++ frontend/src/Pages/Login.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./css/Pages.css";
+import "./css/login.css";
 
 const Login = () => {
   const [id, setId] = useState();
@@ -18,29 +18,29 @@ const Login = () => {
   };
 
   return (
-    <div className="div">
-      <header id="header" className="header">
-        <h1 id="page-title" className="page-title">
-          예약 현황 관리자 페이지 로그인
-        </h1>
+    <div className="login__wrapper">
+      <div className="login">
+      <header className="login__header">
+        <h1 className="login__title">예약 현황 관리자 페이지 로그인</h1>
       </header>
 
-      <main id="main" className="main">
-        <label htmlFor="admin-id" className="input-label">
+      <main className="login__main">
+        <label htmlFor="admin-id" className="login__label">
           ID:
-          <input
-            id="admin-id"
-            className="input-field"
-            type="text"
-            placeholder="관리자 아이디 입력"
-            value={id}
-            onChange={(e) => setId(e.target.value)}
-          />
         </label>
-        <button id="login-button" className="action-link" onClick={login}>
+        <input
+          id="admin-id"
+          className="login__input"
+          type="text"
+          placeholder="관리자 아이디 입력"
+          value={id}
+          onChange={(e) => setId(e.target.value)}
+        />
+        <button className="login__button" onClick={login}>
           로그인
         </button>
       </main>
+    </div>
     </div>
   );
 };
