2caaecc932383ac9822f5a2a6e67569a362d97b5
GaYeon <yun02190219@gmail.com>
2025-02-25 03:01:30 +0900

    💄 #39 달력 디자인 수정

---
+++ frontend/src/Components/ReservationAdmin/Reservation/Calendar.js
@@ -0,0 +1,40 @@
+import React from "react";
+import DatePicker from "react-datepicker";
+import { ko } from "date-fns/locale"; // 한글 로캘 추가
+import "react-datepicker/dist/react-datepicker.css";
+import "../css/calendar.css";
+
+const CalendarComponent = ({ selectedDate, setSelectedDate }) => {
+  return (
+    <div className="calendar-container">
+      <DatePicker
+        selected={selectedDate}
+        onChange={(date) => setSelectedDate(date)} // 부모 상태 변경
+        dateFormat="yyyy년 MM월 dd일"
+        locale={ko}
+        className="calendar"
+        renderCustomHeader={({
+          date,
+          decreaseMonth,
+          increaseMonth,
+          prevMonthButtonDisabled,
+          nextMonthButtonDisabled,
+        }) => (
+          <div className="custom-header">
+            <button onClick={decreaseMonth} disabled={prevMonthButtonDisabled}>
+              ◀
+            </button>
+            <span>
+              {date.getFullYear()}년 {date.getMonth() + 1}월
+            </span>
+            <button onClick={increaseMonth} disabled={nextMonthButtonDisabled}>
+              ▶
+            </button>
+          </div>
+        )}
+      />
+    </div>
+  );
+};
+
+export default CalendarComponent;
